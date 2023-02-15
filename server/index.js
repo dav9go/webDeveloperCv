5d5056be7f6ad79646a8cd49de256d955fcae7a1
David <websajang@gmail.com>
2023-02-15 22:58:16 +0700

    15 Feb 2023

--- server/index.js
+++ server/index.js
@@ -6,10 +6,14 @@ import connectDB from "./mongodb/connect.js";
 import { connect } from "mongoose";
 import itemRoutes from "./routes/itemRoutes.js";
 
-dotenv.config();
+dotenv.config({ path: "ENV_FILENAME" });
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/item-routes", itemRoutes);
